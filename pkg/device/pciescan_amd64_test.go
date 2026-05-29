7743a1e85a93b7e2712c32378bcc78f419b493e1
Francesco Romani <fromani@redhat.com>
2026-05-29 11:11:42 +0200

    pcie: hw: add tool to create test data
    add a support tool which we can run to extract the machine
    data from sysfs and create fstest.MapFS data to use in unit test.
    The usage is straightforward:
    ```
    $ go run tools/gen-pcie-testdata/main.go -h
    Usage of /tmp/go-build4024417054/b001/exe/main:
      -build-tag string
        	inject a //go:build constraint for the current architecture, set "" to disable (default "amd64")
      -func string
        	function name for the generated MapFS fixture (default "makeSysfsFixture")
      -pkg string
        	package name for the generated MapFS fixture (default "device")
      -sysfs-root string
        	sysfs root path (default "/sys")
    ```
    
    We intentionally don't wire the tool in the Makefile (e.g. no target)
    because this is meant to be a developer tool, not something users
    consume.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/device/pciescan_amd64_test.go
+++ pkg/device/pciescan_amd64_test.go
@@ -43,6 +43,64 @@ func TestPCIeDomainsFromFS(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "ryzen 9 5950X - single PCIe root",
+			fs:   makeRyzen95950XFixture(),
+			want: []PCIeDomain{
+				{
+					// created manually inspecting the machine hardware.
+					PCIeRootAttr: makePCIeRootAttr("pci0000:00"),
+					LocalCPUs:    mustParseCPUSet(t, "0-31"),
+					NUMANode:     -1, // dev laptop has no real affinity reported
+				},
+			},
+		},
+		{
+			name: "xeon gold 6230R - multiple symmetric PCIe roots",
+			fs:   makeXeonGold6230RFixture(),
+			want: []PCIeDomain{
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:00"),
+					LocalCPUs:    mustParseCPUSet(t, "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102"),
+					NUMANode:     0,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:17"),
+					LocalCPUs:    mustParseCPUSet(t, "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102"),
+					NUMANode:     0,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:3a"),
+					LocalCPUs:    mustParseCPUSet(t, "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102"),
+					NUMANode:     0,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:5d"),
+					LocalCPUs:    mustParseCPUSet(t, "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102"),
+					NUMANode:     0,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:80"),
+					LocalCPUs:    mustParseCPUSet(t, "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103"),
+					NUMANode:     1,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:85"),
+					LocalCPUs:    mustParseCPUSet(t, "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103"),
+					NUMANode:     1,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:ae"),
+					LocalCPUs:    mustParseCPUSet(t, "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103"),
+					NUMANode:     1,
+				},
+				{
+					PCIeRootAttr: makePCIeRootAttr("pci0000:d7"),
+					LocalCPUs:    mustParseCPUSet(t, "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103"),
+					NUMANode:     1,
+				},
+			},
+		},
 	}
 
 	for _, tt := range tests {
